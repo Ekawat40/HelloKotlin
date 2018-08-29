02b1f7b2bba0de90816e99d4500556a84c1ab7ba
Ekawat40 <aekwatt@hotmail.com>
2018-08-29 17:52:40 +0700

    Variable

---
+++ src/Variable.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>){
+    var hello = "Hello World !"
+    var firstname = "Ekawat"
+    var lastname = "Pat"
+    var test = "eiei"
+    //println(hello) // ctrl+/ or // or /* */
+    println(firstname+" "+lastname)
+}
