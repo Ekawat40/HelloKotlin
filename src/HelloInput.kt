1a6995656a0b0a98aadfe52ebbf9136b0589415b
Ekawat40 <aekwatt@hotmail.com>
2018-08-29 19:13:43 +0700

    Input

---
+++ src/HelloInput.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    println("Your First Name : ")
+    var name = readLine()
+    println("Your Last Name : ")
+    var lastName = readLine()
+    println("Ypur NickName : ")
+    var nickName = readLine()
+    println("Your Address : ")
+    var address = readLine()
+
+//    println("My name is " + name +" "+ lastName+ " my Nickname : " +nickName + " i live in "+ address)
+    println("Hello ! My name is $name $lastName Everyone call me $nickName and i live in $address")
+}
