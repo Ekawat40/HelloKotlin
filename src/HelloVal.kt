f36489f4b4ff15575d8a9bd27cc76dd18361a5fc
Ekawat40 <aekwatt@hotmail.com>
2018-08-29 18:02:08 +0700

    Val

---
+++ src/HelloVal.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var number = 20
+    val number2 = 30
+    val pi = 3.14
+    val domain = "www.google.com"
+    println(number + number2)
+    number = 30
+//    number2 = 50 Error
+    println(number + number2)
+}
