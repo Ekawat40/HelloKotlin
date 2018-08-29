f7d067a255da157bddc3159704a5cccb531e0b88
Ekawat40 <aekwatt@hotmail.com>
2018-08-29 17:55:05 +0700

    Int

---
+++ src/HelloInt.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var number: Int = 30
+    var number2: Int = 25
+    println("I'm " + number + number2 + " year old .")
+    println("I'm " + (number + number2) + " year old .")
+    println("i'm $number year old .")
+}
