ea479b5b1b7e544fbad5e13e3000e81cf75083a6
Ekawat40 <aekwatt@hotmail.com>
2018-08-30 09:42:45 +0700

    cast data type

---
+++ src/HelloConvertType2.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    println("Hello !")
+//    var price = "10".toInt()
+    var price = "10"
+    var total = price.toFloat()+(price.toFloat() * 7 / 100)
+    println(total)
+}
