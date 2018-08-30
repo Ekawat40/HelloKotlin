167fed6b518ac5aa0936596eef8ac7aecaa8d59a
Ekawat40 <aekwatt@hotmail.com>
2018-08-30 09:41:12 +0700

    increment and decrement

---
+++ src/HelloDeCre.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    var num1 = 5
+    var num2 = 1
+    num1++
+    //num1 = num1 + 1
+
+    num1--
+    //num1 = num1 - 1
+
+    println(num1++)
+
+    println(++num1)
+
+    var num3 = num1 +num2
+    num3 += 10
+    //num3= num3 +10
+}
